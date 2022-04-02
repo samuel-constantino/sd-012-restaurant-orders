f2729ba5cfb5fbc36db9dc00508ea30f56138e51
Samuel Constantino <samuelicapuidados@gmail.com>
2022-04-02 09:53:23 -0300

    implementa requisito 1

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,2 +1,52 @@
+from reader_csv import reader_csv
+
+
+def get_values_list_key(dict_list, key):
+    values_list = [item[key] for item in dict_list]
+    return set(values_list)
+
+
+def get_frequency_key_person(dict_list, key, person):
+    dict_frequency = {}
+    for item in dict_list:
+        if item['name'] == person:
+            if item[key] not in dict_frequency:
+                dict_frequency[item[key]] = 1
+            else:
+                dict_frequency[item[key]] += 1
+    return dict_frequency
+
+
 def analyze_log(path_to_file):
-    raise NotImplementedError
+    dict_list = reader_csv(path_to_file)
+
+    foods_list = get_values_list_key(dict_list, 'food')
+    open_days = get_values_list_key(dict_list, 'day')
+
+    food_frequency_maria = get_frequency_key_person(dict_list, 'food', 'maria')
+    bigger_frequency_maria_food = max(food_frequency_maria.values())
+    most_frequent_maria_food = next(
+        food[0]
+        for food in food_frequency_maria.items()
+        if food[1] == bigger_frequency_maria_food
+    )
+
+    food_frequency_arnaldo = get_frequency_key_person(dict_list, 'food', 'arnaldo')
+    food_frequency_joao = get_frequency_key_person(dict_list, 'food', 'joao')
+    day_frequency_joao = get_frequency_key_person(dict_list, 'day', 'joao')
+
+    difference_joao = foods_list.difference(food_frequency_joao)
+    days_difference_joao = open_days.difference(day_frequency_joao)
+
+    infos = [
+        most_frequent_maria_food,
+        str(food_frequency_arnaldo['hamburguer']),
+        str(difference_joao),
+        str(days_difference_joao)
+    ]
+
+    break_line_infos = "\n".join(infos)
+
+    with open('data/mkt_campaign.txt', 'w') as file:
+        for item in break_line_infos:
+            file.writelines(item)
